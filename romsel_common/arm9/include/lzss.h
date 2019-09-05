97af7eddd6402e2ce4e717ae8b7acf1d42ff6322
David Korth <gerbilsoft@gerbilsoft.com>
2019-09-05 00:29:08 -0400

    Moved lzss.c to romsel_common.
    Used in both romsel_dsimenutheme and title.
    
    The only difference between the two was an #include path.

---
+++ romsel_common/arm9/include/lzss.h
@@ -0,0 +1,14 @@
+#ifndef __ROMSEL_COMMON_LZ77_DECOMPRESS_H__
+#define __ROMSEL_COMMON_LZ77_DECOMPRESS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void LZ77_Decompress(u8* source, u8* destination);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ROMSEL_COMMON_LZ77_DECOMPRESS_H__ */
